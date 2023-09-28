332b89db495f4d4b3f18a65ff1cfe2b464d041cf
Selahattin DİNDAŞ <137459338+selahattindindas@users.noreply.github.com>
2023-09-28 17:56:09 +0300

    feat: first commit.

--- moviesapp/src/app/services/common/models/movies.service.ts
+++ moviesapp/src/app/services/common/models/movies.service.ts
@@ -49,6 +49,7 @@ export class MoviesService {
     this.httpClientService.put({
       controller: "Movie",
       action: id
+      
     }, movie).subscribe(
       () => {
         this.sweetalertService.showAlert(MessageTitle.Success, MessageText.UpdateSuccess, icon.Success ,false, ConfirmButtonText.Okey, 3);
