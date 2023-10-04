3fbfa4e6d0af6989d1d6e4fb8b6730d346c2aba2
Selahattin DİNDAŞ <137459338+selahattindindas@users.noreply.github.com>
2023-10-04 05:16:09 +0300

    feat: director post commit.

--- moviesapp/src/app/contracts/movie/list-movie.ts
+++ moviesapp/src/app/contracts/movie/list-movie.ts
@@ -11,7 +11,6 @@ export class List_Movie{
 
 constructor(id:string, name:string, categoryName:string, platformName:string, releaseDate:Date, movieTime:string, description:string,directors:string[], players:string){
     this.id = id;
-    
     this.name = name,
     this.categoryName = categoryName,
     this.platformName = platformName,
